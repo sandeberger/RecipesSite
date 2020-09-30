9ee3b3af7a654e66cc881df0b0849a48970e8f85
Joakim Sandeberger <joakim.sandeberger@gmail.com>
2020-09-30 19:06:01 +0200

    Rättningar men ej i ok skick

--- CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ CourseProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,12 +7,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() RecipeWasSelected = new EventEmitter<Recipe>();
   //@ViewChild ('recipes'){static: true} recipes: ElementRef;
-  //Recipe[] = [new Recipe('A test recipe', 'Good food for you','https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?resize=970%2C546&ssl=1')];
+  //recipes: ElementRef;
+  receipe: Recipe[] = [new Recipe('A test recipe', 'Good food for you','https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg?resize=970%2C546&ssl=1')];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.RecipeWasSelected.emit(recipe);
   }
 
 }
